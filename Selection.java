355ec53d823dcee45fcfb86904b1e52a02303067
Jun Bum Kim <55865272+KimJunBum314@users.noreply.github.com>
2019-10-17 00:18:30 -0400

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -2,10 +2,11 @@
 /**
  * General Selection Sort
  * 
- *
- * 
- * @author Jun Kim, kim2674
+ * @author Jun Kim
  * @version 10/15/2019
+ *
+ * General Worse case runtime O(n^2)
+ * 
  *
  */
 public class Selection extends Sort {
@@ -23,13 +24,15 @@ public class Selection extends Sort {
 		for (int i = 0; i < a.length; i++) {
 			int place = i;
 			Comparable min = a[i];
-
+			
+			// Look through the i+1 to N to find min
 			for (int j = i+1; j < a.length; j++) {
 				if (less(a[j], min)) {
 					min = a[j];
 					place = j;
 				}
 			}
+			// Switch elements at the i  and j spot 
 			exch(a, i, place);
 		}
 
